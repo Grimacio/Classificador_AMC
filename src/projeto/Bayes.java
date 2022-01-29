9d0c6fef833711446865830ed88e4204a57ff82b
mmpdinis <75619204+mmpdinis@users.noreply.github.com>
2022-01-29 22:50:37 +0000

    Update Bayes.java

--- src/projeto/Bayes.java
+++ src/projeto/Bayes.java
@@ -11,6 +11,9 @@ public class Bayes implements Serializable{
 	private static final long serialVersionUID=1L;
 	private Floresta tree;
 	private double[][][] tensor;
+	
+
+// FUNCOES SUGERIDAS
 
 	public Bayes() {
 		super();
@@ -27,7 +30,7 @@ public class Bayes implements Serializable{
 	
 //cria uma rede de Bayes através de uma floresta, uma amostra e uma pseudo-contagem	
 //O(nds)
-	public double[][][] tensorConstructor(Floresta floresta, Amostra amostra, double s) {
+	private double[][][] tensorConstructor(Floresta floresta, Amostra amostra, double s) {
 		
 		double[][][] Tensor = new double[floresta.size()][][];
 		for (int i = 0; i < floresta.size(); i=i+1) {
@@ -40,70 +43,27 @@ public class Bayes implements Serializable{
 		}
 		return Tensor;
 	}
-	//O(1)
-	public double[][][] getTensor() {
-		return tensor;
-	}
-	//O(1)
-	public void setTree(Floresta tree) {
-		this.tree = tree;
-	}
-	//O(1)
-	public void setTensor(double[][][] tensor) {
-		this.tensor = tensor;
-	}
-	//O(1)
-	public Floresta getTree() {
-		return tree;
-	}
-	
 
-	public void readBayes(String path) { // iii
-		try {
-			ObjectInputStream ois = new ObjectInputStream(new FileInputStream(path));
-			Bayes rede = (Bayes) ois.readObject();
-			setTensor(rede.getTensor());
-			setTree(rede.getTree());
-			ois.close();
-		} catch (Exception e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-	}
-	
-	public void writeBayes(String path) { 
-		try {
-			ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream(path));
-			oos.writeObject(this);
-			oos.close();
-		} catch (Exception e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-	}
 	//O(r*n)
 	private double[][] matrixCondRoot(Amostra amostra,int root, double s) {
-		double[][] newMatrix= new double[1][amostra.domain()[root]];
+		double[][] newMatrix= new double[1][amostra.domain(root)];
 		for(int j=0; j<newMatrix[0].length;j=j+1) {
-//			int[] vars= {root};
-//			int[] varsValue = {j};
-			double intersecao;//= amostra.count(vars, varsValue);
-			intersecao = (double) amostra.getCountTensor()[0][root][amostra.domain()[0]][j];
-			newMatrix[0][j] = (intersecao+s)/(amostra.length()+s*amostra.domain()[root]);
+			double intersecao;
+			intersecao = (double) amostra.getCountTensor()[0][root][amostra.domain(0)][j];
+			newMatrix[0][j] = (intersecao+s)/(amostra.length()+s*amostra.domain(root));
 		}
 	return newMatrix;
 	}
 	
 	//O(d*s)
 	private double[][] matrixCond(Amostra amostra,int son, int daddy, double s) {
-		//System.out.println(daddy+" new daddy ");
-		double[][] newMatrix= new double[amostra.domain()[daddy]][amostra.domain()[son]];
+		double[][] newMatrix= new double[amostra.domain(daddy)][amostra.domain(son)];
 		for(int i=0;i<newMatrix.length;i=i+1) {
 			int daddyCount;
 			if(daddy==0) {
-				daddyCount= amostra.getCountTensor()[daddy][1][i][amostra.domain()[1]];
+				daddyCount= amostra.getCountTensor()[daddy][1][i][amostra.domain(1)];
 			}else {
-				daddyCount = amostra.getCountTensor()[0][daddy][amostra.domain()[0]][i];
+				daddyCount = amostra.getCountTensor()[0][daddy][amostra.domain(0)][i];
 			}
 			for(int j=0; j<newMatrix[0].length;j=j+1) {
 				newMatrix[i][j]=DFO(amostra, son, daddy, j , i, daddyCount, s);
@@ -119,11 +79,10 @@ public class Bayes implements Serializable{
 		}else {
 			intersecao= (double) amostra.getCountTensor()[daddy][son][daddyValue][sonValue];
 		}
-		return (intersecao+s)/(daddyCount+s*amostra.domain()[son]);
+		return (intersecao+s)/(daddyCount+s*amostra.domain(son));
 	}
 	
-	
-// WHAT IS THIS 
+
 //O(n);
 	public double prob(int[] vector, double s) {
 		int[] vTree= this.tree.getForest();
@@ -153,22 +112,67 @@ public class Bayes implements Serializable{
 		prob=prob* tensor[i][0][vector[i]];
 		return prob;
 	}
+	
+	
+	
+// FUNCOES EXTRA
+		
 	//O(1)
 	public int dimClass() {
 		return tensor[tensor.length-1][0].length;
 	}
 
+	
+
+// SETTERS, GETTERS & TO STRING
+	
+	//O(1)
+	public double[][][] getTensor() {
+		return tensor;
+	}
+	//O(1)
+	public void setTree(Floresta tree) {
+		this.tree = tree;
+	}
+	//O(1)
+	public void setTensor(double[][][] tensor) {
+		this.tensor = tensor;
+	}
+	//O(1)
+	public Floresta getTree() {
+		return tree;
+	}	
+	
 	@Override
 	public String toString() {
 		return tree + "\nTensor \n" + Arrays.deepToString(tensor) + "]";
 	}
 	
-	public static void main(String[] Args) {
-		Amostra A= new Amostra("bcancer.csv");
-		Grafos G = new Grafos(A.dataDim());
-		G.build(A);
-		Bayes B = new Bayes(G.max_spanning_tree(), A, 0.5);
-		System.out.println(Arrays.deepToString(B.getTensor()));
+	
+// READ & WRITE
+	
+	public void readBayes(String path) { // iii
+		try {
+			ObjectInputStream ois = new ObjectInputStream(new FileInputStream(path));
+			Bayes rede = (Bayes) ois.readObject();
+			setTensor(rede.getTensor());
+			setTree(rede.getTree());
+			ois.close();
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
+	
+	public void writeBayes(String path) { 
+		try {
+			ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream(path));
+			oos.writeObject(this);
+			oos.close();
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}	
 	
 }
